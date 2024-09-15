ae060e68185e13467ffe1f08b01bb2724dbf89fc
llAndroidManll <sahakyansahak404@gmail.com>
2024-09-15 17:27:41 +0400

    Adding Cloud FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+//    alias(libs.plugins.google.gms.google.services)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,6 +62,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +70,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.google.gms:google-services:4.4.2")
+    implementation("com.google.firebase:firebase-firestore:25.1.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,17 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
 }
